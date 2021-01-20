dfe030da6f909c3fac7b99141f89b4dbc6f81d71
aa75654 <69245480+aa75654@users.noreply.github.com>
2021-01-20 18:32:08 +0530

    Update CDG_Process_165_DataSetup.sql

--- CDG_Process_165_DataSetup.sql
+++ CDG_Process_165_DataSetup.sql
@@ -1,7 +1,10 @@
-delete from CDG_PROPERTY_LOG@CDG001T_QICT.CORP.LITEL.COM where bill_yr_mo=201906;
-delete from ADDRESS_MISMATCH_LOG@CDG001T_QICT.CORP.LITEL.COM where bill_yr_mo=201906;
-delete from centurylink_on_xref_sub@CDG001T_QICT.CORP.LITEL.COM where bill_yr_mo=201906 and rownum < 2;
-update major_run@CDG001T_QICT.CORP.LITEL.COM set BILL_YR_MO=201906 where major_id=(select max(major_id) from major_run@CDG001T_QICT.CORP.LITEL.COM);
+delete from CDG_PROPERTY_LOG@CDG001T_QICT.CORP.LITEL.COM where bill_yr_mo=(select bill_yr_mo from major_run@CDG001T_QICT.CORP.LITEL.COM 
+where major_id=(select max(major_id) from major_run@CDG001T_QICT.CORP.LITEL.COM));
+delete from ADDRESS_MISMATCH_LOG@CDG001T_QICT.CORP.LITEL.COM where bill_yr_mo=(select bill_yr_mo from major_run@CDG001T_QICT.CORP.LITEL.COM 
+where major_id=(select max(major_id) from major_run@CDG001T_QICT.CORP.LITEL.COM));
+delete from centurylink_on_xref_sub@CDG001T_QICT.CORP.LITEL.COM where bill_yr_mo=(select bill_yr_mo from major_run@CDG001T_QICT.CORP.LITEL.COM 
+where major_id=(select max(major_id) from major_run@CDG001T_QICT.CORP.LITEL.COM)) and rownum < 2;
+update major_run@CDG001T_QICT.CORP.LITEL.COM set BILL_YR_MO=202007 where major_id=(select max(major_id) from major_run@CDG001T_QICT.CORP.LITEL.COM);
 commit;
 Insert into CDG_PROCESS_LOG@CDG001T_QICT.CORP.LITEL.COM
    (PROCESS_SEQ, PROCESS_ID, PROCESS_PERIOD, START_DT, END_DT, 
